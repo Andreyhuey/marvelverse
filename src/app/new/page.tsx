f9f8b196c393fdfaaf33ddee31ec17a8c88a00f6
Andreyhuey <andreyhuey777@gmail.com>
2023-06-02 17:02:30 +0100

    " "

---
+++ src/app/new/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Page() {
+  return <h1>Marvel Events</h1>;
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="h-screen text-white">
+        <div>Hello</div>
+      </div>
+    </>
+  );
+}
