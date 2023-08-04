1d23e77e27612f6a1a99b99d0a1505c6b5d8c22b
Andreyhuey <andreyhuey777@gmail.com>
2023-08-04 23:12:45 +0100

    get events creators query created

--- src/services/eventsApi.js
+++ src/services/eventsApi.js
@@ -32,6 +32,12 @@ export const eventsApi = createApi({
           `/events/${eventId}/comics?orderBy=${orderBy}&limit=${limit}&ts=1&apikey=47c728e2933b98677639c9ef3bcbed3c&hash=e926e192b0df9aaff901a57cb66e154a`
         ),
     }),
+    getEventCreators: builder.query({
+      query: ({ eventId, orderBy, limit }) =>
+        createRequest(
+          `/events/${eventId}/creators?orderBy=${orderBy}&limit=${limit}&ts=1&apikey=47c728e2933b98677639c9ef3bcbed3c&hash=e926e192b0df9aaff901a57cb66e154a`
+        ),
+    }),
   }),
 });
 
@@ -40,4 +46,5 @@ export const {
   useGetEventDetailsQuery,
   useGetEventCharactersQuery,
   useGetEventComicsQuery,
+  useGetEventCreatorsQuery,
 } = eventsApi;
