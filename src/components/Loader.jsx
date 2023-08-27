5eddee9b6891b298583bc3e57df88351edfdf3f3
Andreyhuey <andreyhuey777@gmail.com>
2023-08-27 23:30:43 +0100

    loader corrected

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -8,7 +8,7 @@ const Loader = () => {
   }, []);
 
   return (
-    <div className="min-h-screen py-10 flex items-center justify-center bg-black fixed top-0 left-0 w-full h-full z-50">
+    <div className="min-h-screen flex items-center justify-center bg-black fixed top-0 left-0 w-full h-full z-50">
       <BeatLoader
         color="#ffff"
         size={13}
