d36a8d2aa9d2a0654fedcb32065c411791e7b495
Andreyhuey <andreyhuey777@gmail.com>
2023-08-29 12:14:08 +0100

    input css added to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./app/store";
+import "./input.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
