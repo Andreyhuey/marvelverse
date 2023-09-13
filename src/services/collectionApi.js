38c981331756463b6fc314ffd378a5e7b2b95884
Andreyhuey <andreyhuey777@gmail.com>
2023-09-13 20:51:06 +0100

    orderby query removed from collection api

--- src/services/collectionApi.js
+++ src/services/collectionApi.js
@@ -1,4 +1,3 @@
-import { create } from "@mui/material/styles/createTransitions";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseUrl = "https://gateway.marvel.com/v1/public";
@@ -10,9 +9,9 @@ export const collectionApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCollectionTerm: builder.query({
-      query: ({ orderBy, limit, offset, searchTerm }) =>
+      query: ({ limit, offset, searchTerm }) =>
         createRequest(
-          `/characters?nameStartsWith=${searchTerm}orderBy=${orderBy}&limit=${limit}&offset=${offset}&ts=1&apikey=${process.env.REACT_APP_API_KEY}&hash=${process.env.REACT_APP_HASH}`
+          `/characters?nameStartsWith=${searchTerm}&limit=${limit}&offset=${offset}&ts=1&apikey=${process.env.REACT_APP_API_KEY}&hash=${process.env.REACT_APP_HASH}`
         ),
     }),
   }),
