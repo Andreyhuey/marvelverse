b8f66e1c962ec5d890c44ff402ee027ff745881a
Andreyhuey <andreyhuey777@gmail.com>
2023-09-17 20:02:42 +0100

    grid for events, characters and series edited

--- src/pages/Characters/CharacterDetails.jsx
+++ src/pages/Characters/CharacterDetails.jsx
@@ -5,7 +5,6 @@ import { useGetCharacterDetailsQuery } from "../../services/charactersApi";
 import { Link } from "react-router-dom";
 import moment from "moment";
 import Loader from "../../components/Loader";
-import ScrollPositionManager from "../../components/ScrollManager";
 import { BiSolidInfoCircle } from "react-icons/bi";
 
 const CharacterDetails = () => {
@@ -26,71 +25,56 @@ const CharacterDetails = () => {
   return (
     <div>
       <>
-        <div className="bg-gray-950 backdrop-blur-lg px-4 md:px-8 lg:px-20 py-10 text-white">
+        <div className="bg-gray-950 backdrop-blur-lg px-4 md:px-8 lg:px-20 py-10 text-white min-h-screen">
           <div>
-            {character &&
-              character?.map((d) => {
-                return (
-                  <>
-                    <ScrollPositionManager scrollKey={`${d.id + d.name}`} />
-                    <div
-                      key={d.id}
-                      className={`flex  flex-col md:flex-row items-center gap-x-5 gap-y-8 w-full`}
-                    >
-                      <div className="flex-1 w-full items-center justify-center flex">
-                        <img
-                          src={d.thumbnail.path && d.thumbnail.path + ".jpg"}
-                          className=""
-                          alt={"...image of " + d.name}
-                        />
-                      </div>
-
-                      <div className="flex-1 w-full">
-                        <h5 className="font-bold text-[26px] text-center md:text-start md:text-[33px] lg:text-[40px] capitalize ">
-                          {d.name}
-                        </h5>
-                        <div className="flex flex-col justify-center items-start gap-2 max-w-lg">
-                          <p className="font-bold text-lg">Overview:</p>
-                          <p className="font-serif">
-                            {d.description ? (
-                              <>
-                                <p className="text-slate-300">
-                                  {HTMLReactParser(d.description)}
-                                </p>
-                              </>
-                            ) : (
-                              <>
-                                <div className="text-slate-300 flex gap-2 items-center italic">
-                                  <BiSolidInfoCircle className="text-red-500" />{" "}
-                                  No description provided by
-                                  <span className="text-red-500">MARVEL</span>
-                                </div>
-                              </>
-                            )}
-                          </p>
-                          <p className="font-semibold">
-                            Modified:{" "}
-                            <span className="text-slate-300">
-                              {moment(d.modified).format("MMM DD, YYYY")}
-                            </span>
-                          </p>
+            {character
+              ? character?.map((d) => {
+                  return (
+                    <>
+                      <div
+                        key={d.id}
+                        className={`flex  flex-col md:flex-row items-center gap-x-5 gap-y-8`}
+                      >
+                        <div className="flex-1 items-center justify-center flex">
+                          <img
+                            src={d.thumbnail.path && d.thumbnail.path + ".jpg"}
+                            className=""
+                            alt={"...image of " + d.name}
+                          />
                         </div>
 
-                        <div className="items-center justify-center flex w-full">
-                          <div className="grid grid-cols-3 gap-y-10 py-6 items-center justify-between w-full">
-                            {d.events.available !== 0 ? (
-                              <Link to={`/characters/${d.id}/${d.name}/events`}>
-                                <div className="bg-slate-900 hover:scale-110 transition duration-300 ease-in-out w-[100px] h-[100px] text-center text-white flex flex-col items-center justify-center font-bold rounded-xl">
-                                  <p className="font-mono text-[#c0bdbd]">
-                                    {d.events.available}
+                        <div className="flex-1">
+                          <h5 className="font-bold text-[26px] text-center md:text-start md:text-[33px] lg:text-[40px] capitalize ">
+                            {d.name}
+                          </h5>
+                          <div className="flex flex-col justify-center items-start gap-2 max-w-lg">
+                            <p className="font-bold text-lg">Overview:</p>
+                            <p className="font-serif">
+                              {d.description ? (
+                                <>
+                                  <p className="text-slate-300">
+                                    {HTMLReactParser(d.description)}
                                   </p>
-                                  <p className="font-mono">Events</p>
-                                </div>
-                              </Link>
-                            ) : (
-                              ""
-                            )}
+                                </>
+                              ) : (
+                                <>
+                                  <div className="text-slate-300 flex gap-2 items-center italic">
+                                    <BiSolidInfoCircle className="text-red-500" />{" "}
+                                    No description provided by
+                                    <span className="text-red-500">MARVEL</span>
+                                  </div>
+                                </>
+                              )}
+                            </p>
+                            <p className="font-semibold">
+                              Modified:{" "}
+                              <span className="text-slate-300">
+                                {moment(d.modified).format("MMM DD, YYYY")}
+                              </span>
+                            </p>
+                          </div>
 
+                          <div className="grid grid-cols-3 py-6 justify-center gap-x-8 items-center">
                             {d.comics.available !== 0 ? (
                               <Link to={`/characters/${d.id}/${d.name}/comics`}>
                                 <div className="bg-slate-900 hover:scale-110 transition duration-300 ease-in-out w-[100px] h-[100px] text-center text-white flex flex-col items-center justify-center font-bold rounded-xl">
@@ -98,6 +82,19 @@ const CharacterDetails = () => {
                                     {d.comics.available}
                                   </p>
                                   <p className="font-mono">Comics</p>
+                                </div>
+                              </Link>
+                            ) : (
+                              ""
+                            )}
+
+                            {d.events.available !== 0 ? (
+                              <Link to={`/characters/${d.id}/${d.name}/events`}>
+                                <div className="bg-slate-900 hover:scale-110 transition duration-300 ease-in-out w-[100px] h-[100px] text-center text-white flex flex-col items-center justify-center font-bold rounded-xl">
+                                  <p className="font-mono text-[#c0bdbd]">
+                                    {d.events.available}
+                                  </p>
+                                  <p className="font-mono">Events</p>
                                 </div>
                               </Link>
                             ) : (
@@ -119,10 +116,10 @@ const CharacterDetails = () => {
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </>
-                );
-              })}
+                    </>
+                  );
+                })
+              : ""}
           </div>
         </div>
       </>
