bbc50b7fd6e553e3a6c55159381ed03aad35f58a
Andreyhuey <andreyhuey777@gmail.com>
2023-09-30 13:40:28 +0100

    Comic pages modified

--- src/pages/Comics/Comics.jsx
+++ src/pages/Comics/Comics.jsx
@@ -6,6 +6,7 @@ import ScrollPositionManager from "../../components/ScrollManager";
 import { BiSolidInfoCircle } from "react-icons/bi";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import { Autocomplete, TextField } from "@mui/material";
+import ComicsComp from "../../components/pageComp/ComicsComp";
 
 const Comics = () => {
   const [orderBy, setOrderBy] = useState(
@@ -220,53 +221,8 @@ const Comics = () => {
             </fieldset>
           </div>
         </div>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-14 gap-x-8 ">
-          {comics?.map((c) => (
-            <div
-              key={c.id}
-              className={` ${isFetching ? "animate-pulse" : ""} `}
-            >
-              <div className="transition-transform transform hover:scale-110 font-mono relative group cursor-pointer py-2">
-                <ScrollPositionManager scrollKey={`${c.id + c.diamondCode}`} />
-                <Link
-                  key={c.id}
-                  to={`/comics/${c.id}/${c.title}`}
-                  className="py-4"
-                >
-                  <div className={` relative`}>
-                    <>
-                      <img
-                        src={c.thumbnail.path + ".jpg"}
-                        className={`${"rounded-xl w-full"}`}
-                        alt={"img of " + c.title}
-                        onLoad={(isFetching) => (
-                          <Loader className="animate-pulse" />
-                        )}
-                      />
-                    </>
 
-                    {c.description ? (
-                      <div className="text-xl font-bold p-2 font-mono absolute bottom-2 left-0 text-green-500 rounded-br-xl rounded-tl-md">
-                        <BiSolidInfoCircle />
-                      </div>
-                    ) : (
-                      <div className="text-xl font-bold p-2 font-mono absolute bottom-2 left-0 text-red-500 rounded-br-xl rounded-tl-md">
-                        <BiSolidInfoCircle />
-                      </div>
-                    )}
-                  </div>
-                  <div className="px-2 pb-2 flex items-center justify-center">
-                    <div
-                      className={`uppercase  font-bold py-2 font-mono text-[#a7a4a4] text-center"`}
-                    >
-                      {c.title}
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            </div>
-          ))}
-        </div>
+        <ComicsComp comics={comics} />
 
         {/* Pagination example */}
 
