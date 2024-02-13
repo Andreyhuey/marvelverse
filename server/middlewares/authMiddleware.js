7b4b7bee8808fbd2d5c3096a0bfb63bddc2dd1af
Andreyhuey <andreyhuey777@gmail.com>
2024-02-13 04:10:07 +0100

    add: middleware for user authorization

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,32 @@
+import { User } from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+
+export const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(" ")[1];
+
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Get user from the token
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401).json({ message: "Not authorized" });
+    }
+  }
+
+  if (!token) {
+    res.status(401).json({ message: "Not authorized, no token" });
+  }
+});
