1bf452f6e85880d95e5acb01fa736829483c749b
Andreyhuey <andreyhuey777@gmail.com>
2024-02-13 04:05:15 +0100

    add: userModel for mongodb schema created

---
+++ server/models/userModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Your full name is required"],
+    },
+    username: {
+      type: String,
+      required: [true, "Your user name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Your email address is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Your password is required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
